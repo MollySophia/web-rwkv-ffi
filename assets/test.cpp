3b39eae410a3abd832b132054ca47374f5930330
Molly Sophia <mollysophia379@gmail.com>
2024-12-06 13:49:56 +0800

    fixup! debug!

--- assets/test.cpp
+++ assets/test.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
+#ifdef _WIN32
+#include <windows.h>
+#else
 #include <dlfcn.h>
+#endif
 #include "web_rwkv_ffi.h"
 
 int main() {
-    // load libweb_rwkv_ffi.dylib
+#ifdef _WIN32
+    HMODULE handle = LoadLibrary("../target/release/web_rwkv_ffi.dll");
+    if (handle == NULL) {
+        std::cerr << "Cannot open library: " << GetLastError() << std::endl;
+        return 1;
+    }
+
+    void (*init)(uint64_t) = (void (*)(uint64_t))GetProcAddress(handle, "init");
+    void (*load_with_rescale)(const char *, uintptr_t, uintptr_t, uintptr_t) = (void (*)(const char *, uintptr_t, uintptr_t, uintptr_t))GetProcAddress(handle, "load_with_rescale");
+    uint16_t (*infer)(const uint16_t *, uintptr_t, struct Sampler) = (uint16_t (*)(const uint16_t *, uintptr_t, struct Sampler))GetProcAddress(handle, "infer");
+#else
     void *handle = dlopen("../target/release/libweb_rwkv_ffi.dylib", RTLD_LAZY);
     if (!handle) {
         std::cerr << "Cannot open library: " << dlerror() << std::endl;
@@ -14,6 +28,7 @@ int main() {
     void (*load_with_rescale)(const char *, uintptr_t, uintptr_t, uintptr_t) = (void (*)(const char *, uintptr_t, uintptr_t, uintptr_t))dlsym(handle, "load_with_rescale");
     uint16_t (*infer)(const uint16_t *, uintptr_t, struct Sampler) = (uint16_t (*)(const uint16_t *, uintptr_t, struct Sampler))dlsym(handle, "infer");
 
+#endif
     init(0);
     load_with_rescale("../rwkv-sudoku.st", 0, 0, 999);
     uint16_t prompt[] = {102, 1, 1, 9, 2, 7, 8, 1, 3, 1, 132, 6, 1, 1, 3, 4, 1, 1, 1, 1, 132, 8, 7, 1, 1, 6, 5, 9, 1, 2, 132, 9, 8, 1, 1, 5, 1, 1, 1, 1, 132, 1, 3, 1, 1, 1, 1, 1, 1, 1, 132, 1, 1, 5, 1, 1, 4, 1, 10, 1, 132, 1, 1, 1, 1, 1, 1, 4, 8, 1, 132, 1, 5, 1, 1, 1, 1, 1, 9, 1, 132, 4, 2, 1, 9, 1, 7, 10, 1, 5, 132, 103};
